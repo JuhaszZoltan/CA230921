b19d4cb6a9bb41b3eebf22032db504985cb82760
juhaszz <juhaszz@201-TANARI>
2023-09-21 12:55:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CA230921
+{
+    class Program
+    {
+        static List<Versenyzo> versenyzok = new List<Versenyzo>();
+
+        static void Main()
+        {
+            // usign: IDisposable interface-re: automatikusan "lezárja" az olvasót
+            using var sr = new StreamReader(
+                @"..\..\..\src\ub2017egyeni.txt",
+                Encoding.UTF8);
+            // _ (discard): memóriába történő tárolás nélkül "eldobja" a sort.
+            _ = sr.ReadLine();
+            while (!sr.EndOfStream) versenyzok.Add(new Versenyzo(sr.ReadLine()));
+
+            Console.WriteLine($"versenyzok szama: {versenyzok.Count}");
+
+            //kódindentáció példány szintű hívási láncoknál:
+            //selektor (a pont) a sor elején legyen!
+            var ffatlag = versenyzok
+                .Where(v => v.Kategoria && v.TavSzazalek == 100)
+                .Average(v => v.IdoOraban);
+            Console.WriteLine($"ferfiak atlagideje: {ffatlag:0.0000} ora");
+
+            Console.WriteLine("verseny gyoztesei:");
+            Console.WriteLine($"\t{Gyoztes(false)}");
+            Console.WriteLine($"\t{Gyoztes(true)}");
+        }
+        static string Gyoztes(bool kategoria)
+        {
+            var gyoztes = versenyzok
+                .Where(v => v.Kategoria == kategoria && v.TavSzazalek == 100)
+                .OrderBy(v => v.Versenyido)
+                .First();
+
+            return $"{(kategoria ? "Ferfiak:" : "Nok:")} " +
+                $"{gyoztes.Nev} " +
+                $"({gyoztes.Rajtszam}.) - " +
+                $"{gyoztes.Versenyido}";
+        }
+    }
+}
---
+++ Versenyzo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA230921
+{
+    class Versenyzo
+    {
+        public string Nev { get; set; }
+        public string Rajtszam { get; set; }
+        public bool Kategoria { get; set; }
+        public TimeSpan Versenyido { get; set; }
+        public int TavSzazalek { get; set; }
+        public double IdoOraban => Versenyido.TotalHours;
+
+        public Versenyzo(string r)
+        {
+            var v = r.Split(';');
+            Nev = v[0];
+            Rajtszam = v[1];
+            Kategoria = v[2] == "Ferfi";
+            var t = v[3].Split(':');
+            Versenyido = new TimeSpan(
+                int.Parse(t[0]),
+                int.Parse(t[1]),
+                int.Parse(t[2]));
+            TavSzazalek = int.Parse(v[4]);
+        }
+    }
+}
